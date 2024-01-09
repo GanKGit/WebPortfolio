2f36fff92987e13d404e9561998560ae0ae97c0a
GanKGit <ganeshk4777@gmail.com>
2024-01-09 16:10:55 -0500

    Update

---
+++ pages/Contact.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header("Contact Us!")
+st.write("Address :")
+st.write("Phone :")
---
+++ pages/Site Help.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.header("Help!")
+st.write("Topic 1")
+st.write("Topic 2")
+
