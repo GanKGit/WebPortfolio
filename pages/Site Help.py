bf342a74bcb344aeb1279683df6ded603b9f86ce
GanKGit <ganeshk4777@gmail.com>
2024-01-12 13:58:05 -0500

    Update with sendmail

--- pages/Site Help.py
+++ pages/Site Help.py
@@ -1,6 +1,9 @@
 import streamlit as st
-import sendEmail
-
+import sendEmail as se
 receiver = st.text_input("Enter your email :")
-st.button("Press to receive help information")
-sendEmail.send_mail("lgk09021984@gmail.com","kywbcshhwemnnjix", receiver, "Help text sent")
+button=st.button("Press to receive help information", key="Submit")
+
+print("Waiting for submit")
+
+if button:
+    se.send_mail("lgk09021984@gmail.com","kywbcshhwemnnjix", receiver, "Help text sent")
